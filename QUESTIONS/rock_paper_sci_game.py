2660ffa3af14eae91b082e5c918a3ff7732b081e
AdarshRaj <adarsh787011@gmail.com>
2025-07-20 02:02:19 +0530

    Corrected the logic of the code.

--- QUESTIONS/rock_paper_sci_game.py
+++ QUESTIONS/rock_paper_sci_game.py
@@ -17,9 +17,9 @@ Either choose the designated number or Write your pick.
     2. Paper
     3.Scissor""")
 while(i < 3):
-    print(f"Round {j}")
+    print(f"\n*****Round {j}*****\n")
     user_choice = input("Enter your choice: ")
-    if(user_choice == "1" or "2" or "3"):
+    if user_choice in ['1','2','3']:
         i += 1
         j += 1
         choice = ["1", "2", "3"]
@@ -27,30 +27,30 @@ while(i < 3):
         if((user_choice == '1' and comp_choice == '2') or (user_choice == '2' and comp_choice == '3') or (user_choice == '3' and comp_choice == '1')):
             print(f"""YOU Selected: {user_choice}
 COMPUTER Selected: {comp_choice}""")
-            print("\nComputer Wins!!!\n")
+            print("Computer Wins!!!\n")
             comp_win += 1
         elif(user_choice == comp_choice):
             print(f"""YOU Selected: {user_choice}
 COMPUTER Selected: {comp_choice}""")
-            print("\nThe match is tie, You both get 1 point\n")
+            print("The match is tie, You both get 1 point\n")
             comp_win += 1
             user_win += 1
         else:
             print(f"""YOU Selected: {user_choice}
 COMPUTER Selected: {comp_choice}""")
-            print("\nUser Wins!!!\n")
+            print("User Wins!!!\n")
             user_win += 1
-        if(user_win < comp_win):
-            print("User Wins The Game.")
-        elif(user_win == comp_win):
-            print("The match is a tie.")
-        else:
-            print("Computer Wins The Game.")
     else:
         print("Error!!! Rerun the program and Select from the given option")
         i -= 1
         input("Enter any key to continue")
         clear_screen()
         continue
+if(user_win < comp_win):
+    print(f"Comp Wins The Game With Total Point: {comp_win}")
+elif(user_win == comp_win):
+    print("The match is a tie.")
+else:
+    print(f"User Wins The Game With Total Point: {user_win}")
 
 
